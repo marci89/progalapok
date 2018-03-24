07c4e22e9a0c3c0734ff8c698773e17247f15836
Kismarczi József <kismarczirobi@gmail.com>
2018-03-24 12:14:41 +0100

    keverés megoldva, grrafikai megjelenítés kezdetleges, eggérrel irányítható a nyúl, de még bárhova léphet.

---
+++ Minigame/RabbitField/Grass.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Grass extends JButton{
+
+    private int img_x;
+    private int img_y;
+
+    public Grass() {
+        Timer timer = new Timer();
+
+        int x = (int) (Math.random()*8);
+        int y = (int) (Math.random()*8);
+
+        while (RabbitField.matrix[x][y] != 0) {
+            x = (int) (Math.random()*8);
+            y = (int) (Math.random()*8);
+        }
+        RabbitField.matrix[x][y] = 8;
+        img_x = x*100;
+        img_y = y*100;
+        this.setIcon(new  ImageIcon("food.png"));
+        this.setBounds(img_x,img_y,100,100);
+        this.setVisible(true);
+        this.setLayout(null);
+
+
+
+
+
+
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked (MouseEvent e) {
+                RabbitField.actualx = img_x;
+                RabbitField.actualy = img_y;
+
+
+            }
+        });
+
+
+        TimerTask task = new TimerTask() {
+
+            public void run() {
+
+                if (RabbitField.actualpos[0] == img_x && RabbitField.actualpos[1] == img_y) {
+                    setVisible(false);
+                }
+                else setVisible(true);
+
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 10, 10);
+
+
+
+    }
+}
